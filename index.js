1e1bbfa619d41c630799491876ca7fa0219752f4
Jerome Taruc <jerometaruc29@gmail.com>
2025-06-20 17:18:38 -0700

    Added PUT /api/product/:id route; Added DELETE /api/product/:id route

--- index.js
+++ index.js
@@ -51,6 +51,37 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
+// Update a product
+// Endpoint: PUT /api/product/:id
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Delete a product
+// Endpoint: Delete /api/product/:id
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product deleted succesfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 // Connect to MongoDB using the URI in .env
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
